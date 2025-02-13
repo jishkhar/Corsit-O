74874388f3a747037d330e04b2ef6ab0c766bafa
“Jishnu <jishnukhargharia11@gmail.com>
2025-02-14 01:03:58 +0530

    card added

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import jishnu from "../../assets/team/second/jishnu.jpg"
+import Card from './Card.jsx';
 
 const Team = () => {
   return (
     <>
-      Team
-    </>
-  )
-}
+      <div className="text-center text-5xl">
+        Our Team
+      </div>
 
-export default Team
+
+      <div id="core" className="flex justify-center gap-40 flex-wrap">
+        <Card img={jishnu} name="Jatin" position="Chairman" linkedin="jishnu" />
+        <Card img={jishnu} name="Ujjawal" position="Vice-Chairman" linkedin="jishnu" />
+      </div>
+
+
+      <Card img={jishnu} name="Jishnu Khargharia" position="2nd Year" linkedin="jishnu" />
+
+    </>
+  );
+};
+
+export default Team;

17cc1aeab92fa76452edec5a48c0ad2e9e34908a
Jishnu <jishnukhargharia11@gmail.com>
2025-02-15 20:45:03 +0530

    add projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+
+
 const Projects = () => {
   return (
     <>
-      Projects
+      <div className='flex justify-center items-center w-[80%] mx-auto gap-72'>
+        <h1 className='text-4xl font-bold'>Bots & Projects</h1>
+        <div>
+          Group projects thrive on synergy, combining diverse skills to drive innovation and boost efficiency. Collaboration enables individuals to contribute their expertise toward shared goals, resulting in meaningful outcomes. A unified vision fosters cohesion and alignment, ensuring ambitious objectives are achieved with greater impact.
+        </div>
+      </div>
+
+
+      <div id="projects">
+        
+      </div>
     </>
   )
 }

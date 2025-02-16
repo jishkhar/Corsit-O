6b12244bf507696fd3769349f9c754c57b7e2517
Jishnu <jishnukhargharia11@gmail.com>
2025-02-16 14:38:36 +0530

    header modify

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
     ];
 
     return (
-        <header className='bg-[#171717]'>
+        <header className='bg-[#171717] h-20'>
             <nav className='flex justify-between items-center w-[92%] mx-auto py-6'>
                 <div>
                     <Link to='/'>
@@ -29,13 +29,13 @@ const Header = () => {
 
                 <div>
                     <div className={`lg:static absolute lg:w-auto w-full lg:min-h-fit min-h-[60vh] left-0 ${navOpen ? 'top-[10%]' : 'top-[-100%]'}`}>
-                        <ul className='flex lg:flex-row flex-col items-center lg:gap-[2vw] gap-10 text-[1.3rem]'>
+                        <ul className='flex lg:flex-row flex-col items-center lg:gap-[2vw] gap-10 text-[1.2rem]'>
                             {navLinks.map(({ path, label }, index) => (
                                 <li key={index}>
                                     <NavLink 
                                         to={path} 
                                         className={({ isActive }) => 
-                                            `block py-2 pr-4 pl-3 text-[1.375rem] duration-200 ${isActive ? 'text-red-600' : 'text-white'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-600 lg:p-0`
+                                            `block py-2 pr-4 pl-3 text-[1.2rem] duration-200 ${isActive ? 'text-red-600' : 'text-white'} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-red-600 lg:p-0`
                                         }
                                     >
                                         {label}

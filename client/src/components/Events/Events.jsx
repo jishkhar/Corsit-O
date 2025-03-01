846b14b28ee224f9cf51a89f31f8026717d4ebbc
Jishnu <jishnukhargharia11@gmail.com>
2025-03-01 12:23:01 +0530

    start event

--- client/src/components/Events/Events.jsx
+++ client/src/components/Events/Events.jsx
@@ -4,7 +4,11 @@ const Events = () => {
   return (
     <>
       <div className='h-screen bg-[#272928] flex py-40 justify-center'>
-        Event
+      <div className="max-w-3xl">
+          <h2 className="text-5xl font-bold text-[#ed5a2d] tracking-tight sm:text-6xl ">
+            Our Events
+          </h2>
+        </div>
       </div>
     </>
   )
